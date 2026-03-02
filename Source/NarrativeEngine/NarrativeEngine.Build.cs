21c3209aa61801f542b8b9a4f768d55cc9f39525
Not Clayton <claystamper@gmail.com>
2026-03-02 12:05:22 -0600

    - Painted a new dirt path leading out of the starting area - Updated the MM_Landscape shader to subtract dirt from the 'Flowers_Light' landscape layer mask, which was spawning landscape grass on new paths. This will need a more extensible solution in the future

--- Source/NarrativeEngine/NarrativeEngine.Build.cs
+++ Source/NarrativeEngine/NarrativeEngine.Build.cs
@@ -39,6 +39,7 @@ public class NarrativeEngine : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"WaveFunctionCollapse",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
